2aab8728ee3fdf5716a698a052bcf075fada0c62
JOEL ALVES PEREIRA <atejap05@yahoo.com.br>
2022-09-11 21:29:50 -0400

    up

--- src/pdfMake/geraPDF.jsx
+++ src/pdfMake/geraPDF.jsx
@@ -31,6 +31,7 @@ const geraPDF = folhaObj => {
   };
 
   const margin = convertToPdfMakerUnit(0.4);
+  const fontSizeSubHeader = 11;
 
   const { head, body } = folhaObj;
 
@@ -96,6 +97,7 @@ const geraPDF = folhaObj => {
             },
             {
               margin: [60, 5, 0, 0],
+              fontSize: fontSizeSubHeader,
               border: [false, true, true, false],
               text: `CPF: ${head.pessoalInfo.cpf}`,
             },
@@ -103,50 +105,59 @@ const geraPDF = folhaObj => {
           [
             {
               border: [true, false, false, false],
+              fontSize: fontSizeSubHeader,
               text: `Cargo: ${head.pessoalInfo.cargo}`,
             },
             {
               margin: [60, 0, 0, 0],
               border: [false, false, true, false],
+              fontSize: fontSizeSubHeader,
               text: `Siape: ${head.pessoalInfo.siape}`,
             },
           ],
           [
             {
               border: [true, false, false, false],
+              fontSize: fontSizeSubHeader,
               text: `Unidade de Exercício: ${head.pessoalInfo.unidadeExercicio}`,
             },
             {
               margin: [60, 0, 0, 0],
               border: [false, false, true, false],
+              fontSize: fontSizeSubHeader,
               text: `SiapeÚnico: ${head.pessoalInfo.siapeUnico}`,
             },
           ],
           [
             {
               border: [true, false, false, false],
+              fontSize: fontSizeSubHeader,
               text: `Setor de Exercício: ${head.pessoalInfo.setorExercicio}`,
             },
             {
               margin: [60, 0, 0, 0],
               border: [false, false, true, false],
+              fontSize: fontSizeSubHeader,
               text: `SiapeCad: ${head.pessoalInfo.siapeCad}`,
             },
           ],
           [
             {
               border: [true, false, false, false],
+              fontSize: fontSizeSubHeader,
               text: `Unidade de Localização Física: ${head.pessoalInfo.localizacaoFisica.unidade}`,
             },
             {
               margin: [60, 0, 0, 0],
               border: [false, false, true, false],
+              fontSize: fontSizeSubHeader,
               text: `Telefone:  ${head.pessoalInfo.telefone}`,
             },
           ],
           [
             {
               border: [true, false, false, true],
+              fontSize: fontSizeSubHeader,
               text:
                 `Município de Localização Física: ` +
                 `${head.pessoalInfo.localizacaoFisica.municipio}-` +
@@ -155,6 +166,7 @@ const geraPDF = folhaObj => {
             {
               margin: [60, 0, 0, 0],
               border: [false, false, true, true],
+              fontSize: fontSizeSubHeader,
               text:
                 `Pav: ${head.pessoalInfo.localizacaoFisica.complemento.pav} ` +
                 `Sala:  ${head.pessoalInfo.localizacaoFisica.complemento.sala}`,
@@ -177,58 +189,58 @@ const geraPDF = folhaObj => {
   const bodyRows = body.map(row => {
     return [
       {
-        text: row.dia,
-        style: "tableHeader",
+        text: String(row.dia).padStart(2, "0"),
         fontSize: 10,
-        margin: [0, 2, 0, 2],
+        alignment: "center",
+        // margin: [0, 2, 0, 2],
       },
       {
         text: row.entradaManha,
-        style: "tableHeader",
         fontSize: 10,
-        margin: [0, 2, 0, 2],
+        alignment: "center",
+        // margin: [0, 2, 0, 2],
       },
       {
         text: row.saidaManha,
-        style: "tableHeader",
         fontSize: 10,
-        margin: [0, 2, 0, 2],
+        alignment: "center",
+        // margin: [0, 2, 0, 2],
       },
       {
         text: row.entradaTarde,
-        style: "tableHeader",
         fontSize: 10,
-        margin: [0, 2, 0, 2],
+        alignment: "center",
+        // margin: [0, 2, 0, 2],
       },
       {
         text: row.saidaTarde,
-        style: "tableHeader",
         fontSize: 10,
-        margin: [0, 2, 0, 2],
+        alignment: "center",
+        // margin: [0, 2, 0, 2],
       },
       {
         text: row.assinaturaOcorrencia,
-        style: "tableHeader",
         fontSize: 10,
-        margin: [0, 2, 0, 2],
+        alignment: "center",
+        // margin: [0, 2, 0, 2],
       },
       {
         text: row.horas,
-        style: "tableHeader",
         fontSize: 10,
-        margin: [0, 2, 0, 2],
+        alignment: "center",
+        // margin: [0, 2, 0, 2],
       },
       {
         text: row.compensacao,
-        style: "tableHeader",
         fontSize: 10,
-        margin: [0, 2, 0, 2],
+        alignment: "center",
+        // margin: [0, 2, 0, 2],
       },
       {
         text: row.abono,
-        style: "tableHeader",
         fontSize: 10,
-        margin: [0, 2, 0, 2],
+        alignment: "center",
+        // margin: [0, 2, 0, 2],
       },
     ];
   });
@@ -237,31 +249,67 @@ const geraPDF = folhaObj => {
   const details = [
     {
       table: {
-        headerRows: 1,
-        // columns widths (4 cols)
-        // widths: [10, 50, 100, 90],
-        // * automatic adjustment
-        widths: [20, "*", "*", "*", "*", "*", "*", "*", "*"],
+        widths: [20, 40, 40, 40, 40, 177, 35, 35, 35],
         body: [
           [
-            { text: "Dia", style: "tableHeader", fontSize: 10 },
-            { text: "Entrada", style: "tableHeader", fontSize: 10 },
-            { text: "Saída", style: "tableHeader", fontSize: 10 },
-            { text: "Entrada", style: "tableHeader", fontSize: 10 },
-            { text: "Saída", style: "tableHeader", fontSize: 10 },
             {
+              alignment: "center",
+              text: "Dia",
+              style: "tableHeader",
+              fontSize: 12,
+            },
+            {
+              alignment: "center",
+              text: "Entrada",
+              style: "tableHeader",
+              fontSize: 12,
+            },
+            {
+              alignment: "center",
+              text: "Saída",
+              style: "tableHeader",
+              fontSize: 12,
+            },
+            {
+              alignment: "center",
+              text: "Entrada",
+              style: "tableHeader",
+              fontSize: 12,
+            },
+            {
+              alignment: "center",
+              text: "Saída",
+              style: "tableHeader",
+              fontSize: 12,
+            },
+            {
+              alignment: "center",
               text: "Assinatura/Ocorrência",
               style: "tableHeader",
-              fontSize: 10,
+              fontSize: 12,
             },
-            { text: "Hora", style: "tableHeader", fontSize: 10 },
-            { text: "Comp.", style: "tableHeader", fontSize: 10 },
-            { text: "Abono", style: "tableHeader", fontSize: 10 },
+            {
+              alignment: "center",
+              text: "Hora",
+              style: "tableHeader",
+              fontSize: 12,
+            },
+            {
+              alignment: "center",
+              text: "Comp.",
+              style: "tableHeader",
+              fontSize: 12,
+            },
+            {
+              alignment: "center",
+              text: "Abono",
+              style: "tableHeader",
+              fontSize: 12,
+            },
           ],
           ...bodyRows,
         ],
       },
-      layout: "headerLineOnly",
     },
   ];
 
@@ -281,7 +329,7 @@ const geraPDF = folhaObj => {
     defaultStyle: { font: "LiberationSerif", color: "#012030" },
     pageSize: "A4",
     // left, top, right, bottom
-    pageMargins: [margin, convertToPdfMakerUnit(3.3), margin, margin],
+    pageMargins: [margin, convertToPdfMakerUnit(3.08), margin, margin],
 
     header: [reportTitle],
     content: [details],
